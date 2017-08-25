3b18c0a08b7c630f1c0bce618469130a27e97cb1
Hugo Hernani <hugo@quezmedia.com>
2017-08-25 15:39:05 -0300

    Adding Material and FlexLayout Modules. Also Added Material Icons and Styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,29 @@ import { RouterModule  } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { routes } from './app.route';
-import { AppComponent, CategoriesComponent, QuestionsComponent, TagsComponent } from './components';
+import 'hammerjs';
+import { MaterialModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { routes }   from './app.route';
+import { AppComponent, CategoriesComponent, TagsComponent, QuestionsComponent } from './components';
 import { CategoryService, TagService, QuestionService } from './services';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CategoriesComponent,
-    QuestionsComponent,
-    TagsComponent
+    AppComponent, CategoriesComponent, TagsComponent, QuestionsComponent
   ],
   imports: [
     BrowserModule,
     // Router
     RouterModule.forRoot(routes),
     FormsModule,
-    HttpModule
+    HttpModule,
+
+    //Material
+    MaterialModule,
+    //Flex
+    FlexLayoutModule
   ],
   providers: [
     CategoryService, TagService, QuestionService
