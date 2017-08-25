e34e9856ae07fc56e04a3f98df1d935971301a43
Hugo Hernani <hugo@quezmedia.com>
2017-08-25 15:04:57 -0300

    Adding Project Base Components, Model and Services. Using json-server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule  } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { routes } from './app.route';
+import { AppComponent, CategoriesComponent, QuestionsComponent, TagsComponent } from './components';
+import { CategoryService, TagService, QuestionService } from './services';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoriesComponent,
+    QuestionsComponent,
+    TagsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // Router
+    RouterModule.forRoot(routes),
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [
+    CategoryService, TagService, QuestionService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/index.ts
@@ -0,0 +1,18 @@
+import { AppComponent } from './app/app.component';
+import { CategoriesComponent } from './category/categories.component';
+import { QuestionsComponent } from './question/questions.component';
+import { TagsComponent } from './tag/tags.component';
+
+export {
+    AppComponent,
+    CategoriesComponent,
+    QuestionsComponent,
+    TagsComponent
+};
+
+export default [
+    AppComponent,
+    CategoriesComponent,
+    QuestionsComponent,
+    TagsComponent
+];
